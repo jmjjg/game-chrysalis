1f02072ecc81c5d70bbcc6e79b47dfac6d229528
Christian BUFFIN <christian.buffin@adullact-projet.coop>
2017-08-23 02:33:56 +0200

    Ajout de documentation (jsdoc)

--- src/game-sounds.js
+++ src/game-sounds.js
@@ -1,5 +1,13 @@
 /*global console*/
 /*jslint this*/
+
+/**
+ * Bibliothèque de sons pour les jeux.
+ *
+ * @constructor
+ * @param {Object} sounds Les sons (clé string, objets Audio)
+ * @param {GameStorage} settings L'objet stockant les paramètres de la partie actuelle
+ */
 var GameSounds = function(sounds, settings) {
 	"use strict";
 
@@ -7,6 +15,13 @@ var GameSounds = function(sounds, settings) {
 	this.settings = settings;
 };
 
+/**
+ * Tente de jouer le son demandé si la clé existe et si les sons sont activés dans
+ * les paramètres de la partie.
+ *
+ * @param {String} key La clé à jouer
+ * @returns {undefined}
+ */
 GameSounds.prototype.play = function(key) {
 	"use strict";
 
