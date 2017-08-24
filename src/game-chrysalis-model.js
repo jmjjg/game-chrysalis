626d48809c6af1f49a33c08f49d8b45cd75f14cc
Christian BUFFIN <christian.buffin@adullact-projet.coop>
2017-08-24 02:40:29 +0200

    Suppression de la closure pour la classe GameChrysalisModel.

--- src/game-chrysalis-model.js
+++ src/game-chrysalis-model.js
@@ -1,53 +1,54 @@
-/*global shuffle, window, global, console*/
+/*global shuffle*/
 /*jslint for*/
 
-(function (module) {
+/**
+ * Modèle pour le jeu des crysalides.
+ *
+ * @constructor
+ */
+var GameChrysalisModel = function() {
+	"use strict";
+};
+
+/**
+ *
+ * @param {Integer} columns
+ * @param {Integer} balance
+ * @param {Integer} i
+ * @returns {Boolean}
+ */
+GameChrysalisModel.prototype.extra = function(columns, balance, i) {
 	"use strict";
 
-	/**
-	 * Modèle pour le jeu des crysalides.
-	 *
-	 * @constructor
-	 */
-	var GameChrysalisModel = function() {};
+	var limitLeft = Math.floor(columns/2),limitRight = Math.ceil(columns/2), addLeft, addRight;
 
-	/**
-	 *
-	 * @param {Integer} columns
-	 * @param {Integer} balance
-	 * @param {Integer} i
-	 * @returns {Boolean}
-	 */
-	GameChrysalisModel.prototype.extra = function(columns, balance, i) {
-		var limitLeft = Math.floor(columns/2),limitRight = Math.ceil(columns/2), addLeft, addRight;
+	addLeft = (-1 === balance && i%columns < limitLeft);
+	addRight = (1 === balance && i%columns > limitRight);
 
-		addLeft = (-1 === balance && i%columns < limitLeft);
-		addRight = (1 === balance && i%columns > limitRight);
+	return addLeft || addRight;
+};
 
-		return addLeft || addRight;
-	};
+/**
+ *
+ * @param {Integer} columns
+ * @param {Integer} rows
+ * @param {Integer} targets
+ * @param {Integer} balance
+ * @returns {Array}
+ */
+GameChrysalisModel.prototype.positions = function(columns, rows, targets, balance) {
+	"use strict";
 
-	/**
-	 *
-	 * @param {Integer} columns
-	 * @param {Integer} rows
-	 * @param {Integer} targets
-	 * @param {Integer} balance
-	 * @returns {Array}
-	 */
-	GameChrysalisModel.prototype.positions = function(columns, rows, targets, balance) {
-		var positions = [], i;
-		balance = parseInt(balance, 10);
+	var positions = [], i;
+	balance = parseInt(balance, 10);
 
-		for(i = 0;i<columns*rows;i+=1) {
+	for(i = 0;i<columns*rows;i+=1) {
+		positions.push(i);
+		if(true === this.extra(columns, balance, i)) {
 			positions.push(i);
-			if(true === this.extra(columns, balance, i)) {
-				positions.push(i);
-			}
 		}
+	}
 
-		shuffle(positions);
-		return positions.slice(0, targets);
-	};
-	module.GameChrysalisModel = GameChrysalisModel;
-}('object' === typeof window ? window : global));
+	shuffle(positions);
+	return positions.slice(0, targets);
+};
